7a3b1efe7625a2e7f8e363bb2b98d48ac810913e
hsmoon <47437381+hyeonseokk@users.noreply.github.com>
2022-11-08 13:55:15 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+
+# BART 돌리는 것
 CUDA_VISIBLE_DEVICES=3,4,6,7 nohup python train.py \
 --save_filename /home/mnt/hyeon/2.checkpoints/LFQA/gen_model/psg_top10/bart \
 --lr 1e-05 \
@@ -8,3 +10,17 @@ CUDA_VISIBLE_DEVICES=3,4,6,7 nohup python train.py \
 --num_node 4 \
 --plugins deepspeed_stage_2 \
 --model_type bart > logs/psg_top10-bart.log &
+
+
+# longt5돌리는 것
+CUDA_VISIBLE_DEVICES=2,7 nohup python train.py \
+--save_filename /home/mnt/hyeon/2.checkpoints/LFQA/gen_model/psg_top10/longt5 \
+--lr 1e-05 \
+--gpus 2 \
+--accelerator ddp \
+--batch_size 2 \
+--precision 32 \
+--max_len 1024 \
+--num_node 8 \
+--plugins deepspeed_stage_2 \
+--model_type longt5 > logs/psg_top10-longt5.log &
